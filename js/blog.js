b309501692e9380d9119909fa27d612a51217c24
jekamokan <jekamokan1994@gmail.com>
2023-01-08 20:26:57 +0100

    Responsive part4

--- js/blog.js
+++ js/blog.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   $('.header__burger-btn').click(function (event) {
     $('.header__burger-btn').toggleClass('header__burger-line--close');
   });
+  $('.header__burger-btn').click(function (event) {
+    $('.header__nav-list').toggleClass('header__nav-list--open');
+  });
  });
--- js/team.js
+++ js/team.js
@@ -5,10 +5,25 @@ $(document).ready(function(){
     infinite: true,
     prevArrow:'<button class="portfolio__slider-btn portfolio__arrow-left"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow:'<button class="portfolio__slider-btn portfolio__arrow-right"><img src="images/arrow-right.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 427,
+        settings: {
+          centerMode: true,
+          slidesToShow: 2,
+        }
+      },
+      
+    ]
   });
+  
   
   $('.header__burger-btn').click(function (event) {
     $('.header__burger-btn').toggleClass('header__burger-line--close');
   });
- 
+  $('.header__burger-btn').click(function (event) {
+    $('.header__nav-list').toggleClass('header__nav-list--open');
+  });
 });
+
+
